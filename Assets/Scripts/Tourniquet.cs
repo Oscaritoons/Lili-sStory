b83047e4452bcff0e1d383d289bc9b5d5599b171
Oscaritoons <ovanzetto@noos.fr>
2022-12-19 23:20:15 +0100

    Petite erreur sur le script Tourniquet

---
+++ Assets/Scripts/Tourniquet.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class demoSon : MonoBehaviour
+{
+
+    public bool etat;
+    public Quaternion ouvre;
+    Quaternion ferme;
+    float step;
+    AudioSource MyAudioSource;
+    public AudioClip SoundOpen;
+
+    public BoxCollider ColliderTourniquet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ferme = this.transform.rotation;
+        etat = true;
+        ColliderTourniquet = true;
+        MyAudioSource = GetComponent<AudioSource>();
+    }
+
+	// Update is called once per frame
+	void Update()
+    {
+        if (etat)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, ferme, step);
+        }
+        else
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, ouvre, step);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                etat = !etat;
+                MyAudioSource.clip = SoundOpen;
+                MyAudioSource.Play();
+
+                ColliderTourniquet = false;
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/rotationdoor.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotationdoor : MonoBehaviour
+{
+
+    public float speed;
+    public Quaternion ouvre;
+    Quaternion ferme;
+    float step;
+
+    public bool etat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ferme = this.transform.rotation;
+        etat = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        step = speed * Time.deltaTime; // calculate distance to move
+
+        if (etat)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, ferme, step);
+        }
+        else
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, ouvre, step); 
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            etat = false;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            etat = true;
+        }
+    }
+}
+
+
+
